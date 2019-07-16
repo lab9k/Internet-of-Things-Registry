836d84bb85e53ef0fa1d7f318fb0247dc6f7190f
Jefwillems <jefwillems100@gmail.com>
2019-07-16 11:03:35 +0200

    Add translations to marker component

--- src/components/LeafletMarker/index.js
+++ src/components/LeafletMarker/index.js
@@ -2,27 +2,38 @@ import React from 'react';
 import { Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 import PropTypes from 'prop-types';
+import { injectIntl, intlShape } from 'react-intl';
 import categories from '../../static/categories';
 import './style.scss';
+import messages from './messages';
 
-export const LMarker = (props) => {
+
+const createIcon = (device) => new L.Icon({
+  ...(categories[device.application] || categories.Sensor)
+});
+
+const LMarker = (props) => {
   const device = props.device;
-  const deviceIcon = new L.Icon({
-    ...(categories[device.application] || categories.Sensor)
-  });
+  const deviceIcon = createIcon(device);
   const devicePosition = [device.latitude, device.longitude];
   // TODO: requires translation
+  const {
+    intl: { formatMessage }
+  } = props;
+  const deviceLabel = formatMessage(messages.device);
+  const categoryLabel = formatMessage(messages.category);
+  const typesLabel = formatMessage(messages.types);
   return (
     <Marker position={devicePosition} icon={deviceIcon} key={device.id}>
       <Popup>
         <div className="device-popup">
-          <h3>Apparaat</h3>
+          <h3>{deviceLabel}</h3>
           <div className="device-popup-information">
-            <p className="device-popup-information-label">Category</p>
+            <p className="device-popup-information-label">{categoryLabel}</p>
             <p className="device-popup-information-text">{device.categories.join(', ')}</p>
           </div>
           <div className="device-popup-information">
-            <p className="device-popup-information-label">Types</p>
+            <p className="device-popup-information-label">{typesLabel}</p>
             <p className="device-popup-information-text">{device.types.map((el) => el.name).join(', ') || 'Unknown'}</p>
           </div>
         </div>
@@ -32,5 +43,8 @@ export const LMarker = (props) => {
 };
 
 LMarker.propTypes = {
-  device: PropTypes.object
+  device: PropTypes.object,
+  intl: intlShape.isRequired,
 };
+
+export default injectIntl(LMarker);
