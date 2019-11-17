7d8c2e67ed31f6773e859f4f9dfe9a9ebb8ddd48
Jefwillems <jefwillems100@gmail.com>
2019-11-17 15:24:21 +0100

    get default env vars from environment

--- internals/webpack/getEnvVariables.js
+++ internals/webpack/getEnvVariables.js
@@ -12,6 +12,17 @@ module.exports = () => {
 
   const envs = {
     NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+    LANGUAGES: JSON.stringify(process.env.LANGUAGES),
+    DEFAULT_LOCALE: JSON.stringify(process.env.DEFAULT_LOCALE),
+    CITY_NAME: JSON.stringify(process.env.CITY_NAME),
+    CITY_HEADER_THUMBNAIL: JSON.stringify(process.env.CITY_HEADER_THUMBNAIL),
+    API_ROOT: JSON.stringify(process.env.API_ROOT),
+    MAP_ROOT: JSON.stringify(process.env.MAP_ROOT),
+    MAP_MAX_ZOOM: JSON.stringify(process.env.MAP_MAX_ZOOM),
+    MAP_DEFAULT_ZOOM: JSON.stringify(process.env.MAP_DEFAULT_ZOOM),
+    MAP_CENTER_LATITUDE: JSON.stringify(process.env.MAP_CENTER_LATITUDE),
+    MAP_CENTER_LONGITUDE: JSON.stringify(process.env.MAP_CENTER_LONGITUDE),
+
     ...citySpecificEnvs
   };
 
