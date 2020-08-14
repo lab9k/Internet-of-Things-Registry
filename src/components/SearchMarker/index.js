0a6e1280092d3b6cf505e42c77e0f964bb8fd648
Sander Machado <sander.castanheiramachado@student.hogent.be>
2020-08-14 14:29:37 +0200

    updated searchmarker to use the new icon system

--- src/components/SearchMarker/index.js
+++ src/components/SearchMarker/index.js
@@ -2,17 +2,21 @@ import React from 'react';
 import { Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 import PropTypes from 'prop-types';
-import categories from '../../static/categories';
+import { getMarker } from '../../static/marker';
 
 
-const createIcon = () =>
-  new L.Icon({
-    ...(categories.SearchMarker)
+const createIcon = () => {
+  const marker = getMarker('marker');
+  marker.iconSize = [50, 50];
+  return new L.Icon({
+    ...(marker)
   });
+};
+
 
 const SMarker = (props) => {
+  console.log('placing marker');
   const deviceIcon = createIcon();
-  console.log(deviceIcon);
   const location = {
     lat: props.location[0],
     lng: props.location[1]
