72e760be59f27c468ceee849e901aa944d444c91
Sander Machado <sander.castanheiramachado@student.hogent.be>
2020-08-25 09:12:46 +0200

    fixed a crash on api error

--- src/services/api/iot.js
+++ src/services/api/iot.js
@@ -4,12 +4,15 @@ let devices = null;
 
 export async function getDevices() {
   const devicesList = [...(await readPaginatedData(process.env.API_ROOT))];
-
-  return devicesList.map((d) => ({
-    ...d,
-    latitude: Number.parseFloat(d.latitude),
-    longitude: Number.parseFloat(d.longitude)
-  }));
+  const classes = [];
+  devicesList.forEach((c) => classes.push(Object.assign(new Device(), c)));
+  console.log(classes);
+  // return devicesList.map((d) => ({
+  //   ...d,
+  //   latitude: Number.parseFloat(d.latitude),
+  //   longitude: Number.parseFloat(d.longitude)
+  // }));
+  return classes;
 }
 
 export async function getDevice(id) {
@@ -18,4 +21,22 @@ export async function getDevice(id) {
   }
   const all = await devices;
   return all.find((element) => element.id === id);
+}
+export class Device {
+  constructor(id, title, category, type, dataowner, dataprocessing, link, retention, latitude, longitude) {
+    this.id = id;
+    this.title = title;
+    this.category = category;
+    this.type = type;
+    this.dataowner = dataowner;
+    this.dataprocessing = dataprocessing;
+    this.link = link;
+    this.retention = retention;
+    this.latitude = this.parseGeoLoc(latitude);
+    this.longitude = this.parseGeoLoc(longitude);
+  }
+  parseGeoLoc(float) {
+    if (float === undefined) { return 0; }
+    return Number.parseFloat(float);
+  }
 }
